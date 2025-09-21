5faeab0896db27f81618e73b1835176a57298746
AukeB <afbruinsma@gmail.com>
2025-09-21 20:27:17 +0200

    Updated

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 """Main module"""
+from pathlib import Path
 
-from src.mobile_recipe_builder import RecipePDFBuilder
-
+from src.config_manager import ConfigManager
+from src.mobile_recipe_build import RecipePDFBuilder
 
 def main():
     """
-    Main function    
+    Main function
     """
-    recipe_pdf_builder = RecipePDFBuilder()
+    config_path_mobile_recipes = Path("src/configs/config_mobile_recipes.yaml")
+
+    config_manager = ConfigManager()
+    config_mobile_recipes = config_manager.load_config_file(path=config_path_mobile_recipes)
+
+    recipe_pdf_builder = RecipePDFBuilder(config=config_mobile_recipes)
     recipe_pdf_builder.build()
 
 if __name__ == "__main__":
